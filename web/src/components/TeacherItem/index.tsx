43ce7064c6348bf245dddd68ca6a6b429d8ef733
Philipe Galdino <philipep.galdino@gmail.com>
2020-08-08 21:29:03 -0300

    finished and tested the TeacherList page, using props from TeacherItem component

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,30 +4,40 @@ import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
 
-function TeacherItem() {
+export interface Teacher {
+    id: number;
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string,
+}
+interface TeacherItemProps {
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://media.discordapp.net/attachments/724411849839345705/737730645698019488/unknown_7.png" alt="Philipe Galdino"/>
+                <img src={teacher.avatar} alt={teacher.name}/>
                 <div>
-                    <strong>Philipe Galdino</strong>
-                    <span>Matemática</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                     </div>
             </header>
-            <p>
-                Profissional em calculos, apaixonado por números e tabelas avançadas.
-                <br /> <br />
-                Já mudou o número com calculos. Mostrou para o universo o como é possível calcular a terra plana quando dizem que ela é redonda, e fez mudanças em quase todas as normas da física com calculos extraordinários sobre o mundo moderno.
-                </p>
+
+            <p>{teacher.bio}</p>
                 <footer>
                     <p>
                         Preço/hora
-                        <strong>R$ 80,00</strong>
+                        <strong>R$ {teacher.cost}</strong>
                     </p>
-                    <button type="button">
+                    <a href={`https://wa.me/${teacher.whatsapp}`}>
                         <img src={whatsappIcon} alt="Whatsapp"/>
                         Entrar em contato
-                    </button>
+                    </a>
                 </footer>
         </article>
     )
